4a75a1c1446a658293fd048ed1ec8a9d427dcbb9
Sharleen <136054227+Sharn8@users.noreply.github.com>
2025-06-21 22:30:40 +0300

    Add files via upload

---
+++ practice on introduction.py
@@ -0,0 +1,9 @@
+#this is a single line comment explaining the next line of code.
+age=int(25)#assigning age variable
+height=float(1.75)
+name=str("sharleen")#assigning name variable
+x=f"my name is {name} , I am {age} years old and {height} metres tall"
+print(x)
+a=float(age)
+print (a)
+print(type(height))
